0061cbb3917efb6bf0d0061c028586d02f16ca54
bandithxi <bandithxi@hotmail.com>
2015-04-14 20:57:35 -0700

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit bacd40a0172906c9283f6aba17e8337f46f9ec3f.

--- src/application/BBSendTimelineToUniverse.java
+++ src/application/BBSendTimelineToUniverse.java
@@ -1,20 +1,54 @@
 package application;
 
-public class BBSendTimelineToUniverse implements IButtonBehavior {
+public class BBSendTimelineToUniverse  extends Thread implements IButtonBehavior {
 	private String portNum;
+	private SerialComm serialComm;
+	private enum STATES {ON, OFF};
+	private STATES state;
+	Thread loopy;
 	
 	public BBSendTimelineToUniverse(String portNum) {
 		this.portNum = portNum;
+		this.serialComm = new SerialComm("COM" + portNum);
+		state = STATES.OFF;
+		loopy = new Thread(this);
 	}
 	
+	public void run() {
+		int i = 0;
+		while (i != 1000000000) {
+			i++;
+			System.out.println("1");
+			if (Thread.interrupted()) {
+				return;
+			}
+		}
+		System.out.println("Done");
+	}
 	@Override
 	public void click() {
 		// TODO Auto-generated method stub
-		SerialComm serialComm = new SerialComm("COM" + portNum);
+		 
 		
 		try {
-			serialComm.runTimeStep(null);
-		} catch (Exception e) {
+			switch (state) {
+				case OFF:
+					state = STATES.ON; 
+					loopy.start();
+					//serialComm.runTimeStep(null);
+					break;
+				case ON:
+					state = STATES.OFF;
+					loopy.interrupt();
+					throw new InterruptedException();
+					
+			}
+		} 
+		catch (InterruptedException ie) {
+				System.out.println("OFF");
+				return;
+	    }
+		catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
